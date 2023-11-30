ae76d1b0f2b2fba8ed64cdacd1a8d4ef2bb7a59d
TranVanHuu732002 <Huukoi732002@gmail.com>
2023-11-30 17:54:47 +0700

    fix booking-ui

--- cinema-ui/src/components/booking/MovieSelection.vue
+++ cinema-ui/src/components/booking/MovieSelection.vue
@@ -40,34 +40,7 @@
       </v-expansion-panels>
     </div>
     <div class="card-container">
-      <b-card>
-        <div class="d-flex flex-row" style="padding-bottom: 27px;">
-          <img
-            style="height:150px;width: 100px;border-radius: 0.25rem; margin-right: 12px"
-            class="card-img-top"
-            src="https://image.tmdb.org/t/p/original/yg7B62JJbJrkBwiQrRf8vMDogLB.jpg"
-            alt="Image"
-          />
-          <div>
-            <b-card-text class="mb-2"><strong>Chiếm Đoạt</strong></b-card-text>
-            <b-card-text>2D Phụ Đề</b-card-text>
-          </div>
-        </div>
-
-        <div class="text-justify ">
-          <b-card-text class="mb-2"><strong>Galaxy Cà Mau</strong> - RAP 5</b-card-text>
-          <b-card-text>Suất: <strong>19:15</strong> - Thứ Hai, <strong>27/11/2023</strong></b-card-text>
-        </div>
-        <hr class="hr" />
-        <div class="row">
-          <div class="col-sm-6 align-self-start text-start">
-            <b-card-text class="bold-font">Tổng cộng:</b-card-text>
-          </div>
-          <div class="col-sm-6 align-self-end text-end">
-            <b-card-text class="bold-font">140000đ</b-card-text>
-          </div>
-        </div>
-      </b-card>
+      <CardDetails></CardDetails>
     </div>
   </div>
 </template>
@@ -75,7 +48,8 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import MovieCarousel from '@/components/ExpansionPanels/MovieCarousel.vue';
-import ShowTime from '@/components/ExpansionPanels/ShowTime.vue'
+import ShowTime from '@/components/ExpansionPanels/ShowTime.vue';
+import CardDetails from '@/components/ExpansionPanels/CardDetails.vue';
 const panel=ref([0]);
 </script>
 
