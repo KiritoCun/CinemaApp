794f11e287d3f2dd6273956d446e0a00637ea6fe
cmvn-phunguyen2 <phu.nguyen@classmethod.vn>
2023-12-15 19:07:32 +0700

    Finish MovieSelection FE Data

---
+++ cinema-ui/src/views/booking/payment.vue
@@ -0,0 +1,33 @@
+<template>
+  <LayoutCustomerHomepage>
+    <template v-slot:content>
+      <LayoutBooking :step="step">
+        <template v-slot:stepper-container>
+          <Payment></Payment>
+        </template>
+        <template v-slot:btn-group>
+          <router-link class="btn" :to="decrementStep()" :disabled="step === 1">Quay lại</router-link>
+          <router-link class="btn btn--blue-1" :to="incrementStep()" :disabled="step === 4">Tiếp tục</router-link>
+        </template>
+      </LayoutBooking>
+    </template>
+  </LayoutCustomerHomepage>
+</template>
+
+<script setup lang="ts">
+import Payment from '@/components/booking/Payment.vue';
+
+import { ref } from 'vue';
+
+const step = ref(3);
+
+const incrementStep = () => {
+    step.value += 1;
+    return `/booking`;
+  }
+
+const decrementStep = () => {
+    step.value -= 1;
+    return `/booking/seatSelection`;
+}
+</script>
---
+++ cinema-ui/src/views/booking/seatSelection.vue
@@ -0,0 +1,33 @@
+<template>
+  <LayoutCustomerHomepage>
+    <template v-slot:content>
+      <LayoutBooking :step="step">
+        <template v-slot:stepper-container>
+          <SeatSelection></SeatSelection>
+        </template>
+        <template v-slot:btn-group>
+          <router-link class="btn" :to="decrementStep()" :disabled="step === 1">Quay lại</router-link>
+          <router-link class="btn btn--blue-1" :to="incrementStep()" :disabled="step === 4">Tiếp tục</router-link>
+        </template>
+      </LayoutBooking>
+    </template>
+  </LayoutCustomerHomepage>
+</template>
+
+<script setup lang="ts">
+import SeatSelection from '@/components/booking/SeatSelection.vue';
+
+import { ref } from 'vue';
+
+const step = ref(2);
+
+const incrementStep = () => {
+    step.value += 1;
+    return `/booking/payment`;
+  }
+
+const decrementStep = () => {
+    step.value -= 1;
+    return `/booking/movieSelection`;
+  }
+</script>
